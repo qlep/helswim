a4fec05e1d6ef0bd0cf8c638f812b51918e7d4fe
Gleb Tishchenko <owod1.0@gmail.com>
2020-07-26 14:46:33 +0300

    let CLLocationManagerDelegate issue be caused,  battled and won by adding class extension

--- HelSwim/MapViewController.swift
+++ HelSwim/MapViewController.swift
@@ -10,68 +10,74 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController, CLLocationManagerDelegate {
+class MapViewController: UIViewController {
     // MARK: - Properties
     @IBOutlet weak var mapView: MKMapView!
     let locationManager = CLLocationManager()
+
     var fetchedData = ""
-    
+
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationManager.delegate = self
     }
-    
+
     // MARK: - Actions
     // this puts user location on map
-    
     @IBAction func showUserLocation() {
         // check for location tracing permissions first
         let authStatus = CLLocationManager.authorizationStatus()
-        
+
         if authStatus == .notDetermined {
             locationManager.requestWhenInUseAuthorization()
             return
-        } else if authStatus == .denied || authStatus == .restricted {
+        }
+
+        if authStatus == .denied || authStatus == .restricted {
             showLocationServicesDeniedAlert()
             return
         }
-        
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-        locationManager.startUpdatingLocation()
-        
+
         let region = MKCoordinateRegion(center: mapView.userLocation.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
-        
+
         mapView.setRegion(mapView.regionThatFits(region), animated: true)
-        print(mapView.userLocation.coordinate)
+
+        getCoordinates()
     }
-    
+
     // MARK: - Helper methods
     // handle permission errors
     func showLocationServicesDeniedAlert() {
         let alert = UIAlertController(title: "Paikannuspalvelut pois päältä", message: "Ole hyvä ja salli paikannuspalvelut tälle äpille.", preferredStyle: .alert)
-        
-        let okAction = UIAlertAction(title: "OKo", style: .default, handler: nil)
+
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(okAction)
-        
+
         present(alert, animated: true, completion: nil)
     }
-    
+
+    // get user coordinates
+    func getCoordinates() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
+    }
 }
 
 // MARK: - MKMapViewDelegate
 extension MapViewController: MKMapViewDelegate {
-    
+
 }
 
 // MARK: - CLLocationManagerDelegate
-func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-    print("*** Location manager failed with error: \(error.localizedDescription)")
-}
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("*** Location manager failed with error: \(error.localizedDescription)")
+    }
 
-func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    let newLocation = locations.last!
-    print("*** didUpdateLocations \(newLocation)")
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let newLocation = locations.last!
+        print("*** didUpdateLocations \(newLocation)")
+    }
 }
-
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # helswim
 This project is going to put Helsinki swimming beaches on iOS map near you.
-Get locations and hourly water and air temperatures of your favourite places to be.
+Get locations and hourly water and air temperature updates of your favourite places to be right on your phone!
 Data is provided by UiRaS.
